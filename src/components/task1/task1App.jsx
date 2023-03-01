33d194e57c9f9b074d52cdabee9869a698a2293e
Brandon Nolan <brndalx@gmail.com>
2023-03-01 21:32:50 +0200

    feat:image list, counter

--- src/components/task1/task1App.jsx
+++ src/components/task1/task1App.jsx
@@ -2,37 +2,73 @@ import React, { useState } from "react";
 
 export default function Task1App() {
   let [counter, setCounter] = useState(0);
-
-  const handleAdd = () => {
-    if (counter > 4) {
-      counter = -1;
-    }
-    setCounter(counter + 1);
-  };
-  const handleMinus = () => {
-    if (counter < 1) {
-      counter = 6;
-    }
-    setCounter(counter - 1);
-  };
+  let [pathIndex, setPath] = useState(0);
+  let path = [
+    "./images/1.jpg",
+    "./images/2.jpg",
+    "./images/3.jpg",
+    "./images/4.jpg",
+  ];
 
   return (
     <div className="container-fluid">
       <div className="container">
-        <h1 className="my-5">Task 1</h1>
+        <h1 className="my-5">Task 1 - counter</h1>
 
         <div className="text-center ">
           <h4 className="my-4">
             Counter is: <span className="fw-bold text-success"> {counter}</span>
           </h4>
 
-          <button onClick={handleMinus} className="btn btn-outline-primary">
+          <button
+            onClick={() => {
+              counter <= 0
+                ? setCounter((counter = 5))
+                : setCounter(counter - 1);
+            }}
+            className="btn btn-outline-primary"
+          >
             {" "}
             -
           </button>
-          <button onClick={handleAdd} className="ms-4 btn btn-primary">
+          <button
+            onClick={() => {
+              counter >= 5
+                ? setCounter((counter = 0))
+                : setCounter(counter + 1);
+            }}
+            className="ms-4 btn btn-primary"
+          >
             +
           </button>
+        </div>
+      </div>
+      <div className="container">
+        <h1 className="my-5">Task 2 - images list</h1>
+        <div className="text-center ">
+          <img className="w-100 rounded-4" src={path[pathIndex]} alt="fffff" />
+          <div className="mt-4">
+            <button
+              onClick={() => {
+                const nextIndex = pathIndex === 0 ? 3 : pathIndex - 1;
+                setPath(nextIndex);
+                console.log(nextIndex);
+              }}
+              className="btn btn-outline-primary"
+            >
+              Back
+            </button>
+            <button
+              onClick={() => {
+                const nextIndex = pathIndex === 3 ? 0 : pathIndex + 1;
+                setPath(nextIndex);
+                console.log(nextIndex);
+              }}
+              className="ms-4 btn btn-primary"
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
